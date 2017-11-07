b25bd527b013935636335a7665c7933bf6949245
sanjit <sanjitschouhan@gmail.com>
2017-11-07 21:36:17 +0530

    json log

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ app.set('port', (process.env.PORT || 5000));
 var logs = ['test', 'test1']
 
 app.get("/", function (req, res) {
-    res.write("<html></html>")
-    res.write("<h2>Webhook Running</h2>")
-    res.write("<h4>Logs</h4><ul>")
-    for (var i = 0; i < logs.length; i++) {
-        res.write("<li>" + JSON.stringify(logs[i]) + "</li>")
-    }
-    res.end("</ul>EOF")
+    // res.write("<html></html>")
+    // res.write("<h2>Webhook Running</h2>")
+    // res.write("<h4>Logs</h4><ul>")
+    // for (var i = 0; i < logs.length; i++) {
+    // res.write("<li>" + JSON.stringify(logs[i]) + "</li>")
+    // }
+    // res.end("</ul>EOF")
+    res.send(logs[logs.length - 1])
 })
 
 app.post("/webhook/", function (req, res) {
