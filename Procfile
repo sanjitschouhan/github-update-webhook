73d37cfe78819faf819acf028480b396423bcdea
sanjit <sanjitschouhan@gmail.com>
2017-11-07 21:06:28 +0530

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node --inspect=5858 server.js
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+
+const app = express()
+
+app.set('port', (process.env.PORT || 5000));
+
+var logs = ['test', 'test1']
+
+app.get("/", function (req, res) {
+    res.write("<html></html>")
+    res.write("<h2>Webhook Running</h2>")
+    res.write("<h4>Logs</h4><ul>")
+    for (var i = 0; i < logs.length; i++) {
+        res.write("<li>" + JSON.stringify(logs[i]) + "</li>")
+    }
+    res.end("</ul>EOF")
+})
+
+app.post("webhook", function (req, res) {
+    console.log(req.params)
+    console.log(req)
+    logs.push(req.params)
+})
+
+app.listen(app.get('port'), function () {
+    console.log("Your application is listening on " + app.get('port'));
+});
