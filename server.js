7bbe74237d1876cf9e702730461c7994ba978539
sanjit <sanjitschouhan@gmail.com>
2017-11-07 23:02:20 +0530

    twilio

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const port = process.env.PORT || 5000;
 
 var http = require('http')
 var createHandler = require('github-webhook-handler')
+var twilio = require('twilio');
 var handler = createHandler({ path: '/webhook', secret: 'sans' })
+
+
+var accountSid = 'ACee5d88a31d7337a2ece33ea5769c6589'; // Your Account SID from www.twilio.com/console
+var authToken = '89c55479c58630800bceb4901834e66c';   // Your Auth Token from www.twilio.com/console
+
+var client = new twilio(accountSid, authToken);
 
 http.createServer(function (req, res) {
     handler(req, res, function (err) {
@@ -16,8 +23,14 @@ handler.on('error', function (err) {
 })
 
 handler.on('*', function (event) {
-    console.log('Received a push event for %s to %s',
-        event.payload.repository.name,
-        event.payload.ref)
-    console.log(event)
+    var msg = 'GitHub Log: ' + event.event + ' on '
+        + event.payload.repository.name + ' by '
+        + event.payload.sender.login;
+    console.log(msg)
+
+    client.messages.create({
+        body: msg,
+        to: '+917382978847',  // Text this number
+        from: '+12028043580' // From a valid Twilio number
+    }).then((message) => console.log(message.sid));
 })
