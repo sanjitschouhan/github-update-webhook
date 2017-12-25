649bf584f0921f6a3e6cadbd315c6b79f2e4164c
Sanjit Singh Chouhan <sanjitschouhan@gmail.com>
2017-12-25 15:20:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -27,11 +27,11 @@ function sendMessageToAll(msg) {
 }
 
 handler.on('*', function (event) {
-   var msg = '*' + event.event + '*' 
+   var msg = '## *' + event.event + '*' 
         + '\nRepo: ' + event.payload.repository.name 
       	+ '\nBy: ' + event.payload.sender.login;
   if (event.event=='pull_request') {
-    msg = '*' + event.event + ' #' + event.payload.number + '*' 
+    msg = '## *' + event.event + ' #' + event.payload.number + '*' 
         + '\nRepo: ' + event.payload.repository.name 
       	+ '\nBy: ' + event.payload.sender.login
       	+ '\nTitle: ' + event.payload[event.event].title
